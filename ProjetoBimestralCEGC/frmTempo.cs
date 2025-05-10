a00556498a4ffd252a2757f5efdecd43da2a5ac6
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-05-10 13:30:27 -0300

    Feita a função do botão que converte segundos em HORAS, MINUTOS E SEGUNDOS

--- ProjetoBimestralCEGC/frmTempo.cs
+++ ProjetoBimestralCEGC/frmTempo.cs
@@ -28,7 +28,20 @@ namespace ProjetoBimestralCEGC
 
         private void btnConverter_Click(object sender, EventArgs e)
         {
+            // Verifica se o valor inserido é um número inteiro válido
+            if (int.TryParse(txtSegundos.Text, out int totalSegundos) && totalSegundos >= 0)
+            {
+                int horas = totalSegundos / 3600;
+                int minutos = (totalSegundos % 3600) / 60;
+                int segundos = totalSegundos % 60;
 
+                txtResultado.Text = $"{horas} hora{(horas != 1 ? "s" : "")}, {minutos} minuto{(minutos != 1 ? "s" : "")} e {segundos} segundo{(segundos != 1 ? "s" : "")}";
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número inteiro de segundos válido.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
--- ProjetoBimestralCEGC/obj/Debug/net8.0-windows/ProjetoBimestralCEGC.AssemblyInfo.cs
+++ ProjetoBimestralCEGC/obj/Debug/net8.0-windows/ProjetoBimestralCEGC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa0e313177140568971ecd19096809d408bc235")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
