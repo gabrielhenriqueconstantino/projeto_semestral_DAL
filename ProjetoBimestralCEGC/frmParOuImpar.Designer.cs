1fec449a10c002ab1c352a758bfb8d80b54e19be
Caduu10 <cadu1013@outlook.com>
2025-05-10 21:14:01 -0300

    mudei para vermelho, teste.

--- ProjetoBimestralCEGC/frmParOuImpar.Designer.cs
+++ ProjetoBimestralCEGC/frmParOuImpar.Designer.cs
@@ -42,11 +42,12 @@
             // 
             // panel1
             // 
-            panel1.BackColor = Color.DodgerBlue;
+            panel1.BackColor = Color.Red;
             panel1.Controls.Add(label1);
             panel1.Location = new Point(-1, 0);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(378, 46);
+            panel1.Size = new Size(432, 61);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -54,26 +55,28 @@
             label1.AutoSize = true;
             label1.Font = new Font("Bahnschrift", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(5, 9);
+            label1.Location = new Point(6, 12);
             label1.Name = "label1";
-            label1.Size = new Size(370, 29);
+            label1.Size = new Size(455, 36);
             label1.TabIndex = 0;
             label1.Text = "DESCUBRA OS NÚMEROS PARES";
             // 
             // txtInicial
             // 
-            txtInicial.Location = new Point(101, 119);
+            txtInicial.Location = new Point(115, 159);
+            txtInicial.Margin = new Padding(3, 4, 3, 4);
             txtInicial.Name = "txtInicial";
             txtInicial.PlaceholderText = "Número inicial...";
-            txtInicial.Size = new Size(179, 23);
+            txtInicial.Size = new Size(204, 27);
             txtInicial.TabIndex = 4;
             // 
             // txtFinal
             // 
-            txtFinal.Location = new Point(101, 148);
+            txtFinal.Location = new Point(115, 197);
+            txtFinal.Margin = new Padding(3, 4, 3, 4);
             txtFinal.Name = "txtFinal";
             txtFinal.PlaceholderText = "Número final...";
-            txtFinal.Size = new Size(179, 23);
+            txtFinal.Size = new Size(204, 27);
             txtFinal.TabIndex = 3;
             // 
             // label2
@@ -81,9 +84,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(87, 49);
+            label2.Location = new Point(99, 65);
             label2.Name = "label2";
-            label2.Size = new Size(206, 17);
+            label2.Size = new Size(272, 23);
             label2.TabIndex = 2;
             label2.Text = "Insira um número inicial e um final.";
             // 
@@ -92,9 +95,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ButtonHighlight;
-            label3.Location = new Point(38, 70);
+            label3.Location = new Point(43, 93);
             label3.Name = "label3";
-            label3.Size = new Size(305, 17);
+            label3.Size = new Size(403, 23);
             label3.TabIndex = 2;
             label3.Text = "Dentro deste intervalo, será exibido todos os números";
             // 
@@ -103,18 +106,19 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.ButtonHighlight;
-            label4.Location = new Point(159, 87);
+            label4.Location = new Point(182, 116);
             label4.Name = "label4";
-            label4.Size = new Size(62, 21);
+            label4.Size = new Size(77, 28);
             label4.TabIndex = 2;
             label4.Text = "PARES.";
             // 
             // btnDescobrir
             // 
             btnDescobrir.Font = new Font("Bahnschrift", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnDescobrir.Location = new Point(101, 188);
+            btnDescobrir.Location = new Point(115, 251);
+            btnDescobrir.Margin = new Padding(3, 4, 3, 4);
             btnDescobrir.Name = "btnDescobrir";
-            btnDescobrir.Size = new Size(179, 28);
+            btnDescobrir.Size = new Size(205, 37);
             btnDescobrir.TabIndex = 2;
             btnDescobrir.Text = "DESCOBRIR";
             btnDescobrir.UseVisualStyleBackColor = true;
@@ -124,18 +128,19 @@
             // 
             txtResultado.BorderStyle = BorderStyle.FixedSingle;
             txtResultado.Enabled = false;
-            txtResultado.Location = new Point(12, 235);
+            txtResultado.Location = new Point(14, 313);
+            txtResultado.Margin = new Padding(3, 4, 3, 4);
             txtResultado.Multiline = true;
             txtResultado.Name = "txtResultado";
-            txtResultado.Size = new Size(353, 211);
+            txtResultado.Size = new Size(403, 281);
             txtResultado.TabIndex = 1;
             // 
             // frmParOuImpar
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DimGray;
-            ClientSize = new Size(377, 458);
+            ClientSize = new Size(431, 611);
             Controls.Add(txtResultado);
             Controls.Add(btnDescobrir);
             Controls.Add(label3);
@@ -144,6 +149,7 @@
             Controls.Add(txtFinal);
             Controls.Add(txtInicial);
             Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmParOuImpar";
             Text = "frmParOuImpar";
             panel1.ResumeLayout(false);
--- ProjetoBimestralCEGC/obj/Debug/net8.0-windows/ProjetoBimestralCEGC.AssemblyInfo.cs
+++ ProjetoBimestralCEGC/obj/Debug/net8.0-windows/ProjetoBimestralCEGC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1718b77976b45853b3a87941082ee48cd50e10a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ce23be4f8a8ab55684e02fb46bf72ba0ded19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoBimestralCEGC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
